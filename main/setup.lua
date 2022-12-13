2d6e56e83fed9af149a604e89e1e01423b7f8528
K3vinb5 <kevinborgborges555@gmail.com>
2022-12-13 23:22:30 +0000

    Update setup.lua

--- main/setup.lua
+++ main/setup.lua
@@ -6,24 +6,6 @@ os.loadAPI("BetterDig/lib/client.lua")
 
 print("Made by Kevinb5\nHi, this program will now change your turtle's home coordinates, just wait a bit...")
 
-function getOrientation()
-    loc1 = vector.new(gps.locate(2, false))
-    if not turtle.forward() then
-        for j = 1, 6 do
-            if not turtle.forward() then
-                turtle.dig()
-            else
-                break
-            end
-        end
-    end
-    loc2 = vector.new(gps.locate(2, false))
-    heading = loc2 - loc1
-
-	turtle.back()
-
-    return ((heading.x + math.abs(heading.x) * 2) + (heading.z + math.abs(heading.z) * 3))
-end
 
 local args = {...}
 
@@ -32,7 +14,7 @@ if args[1] == nil or args[2] == nil or args[3] == nil or args[1] == "help" then
     return
 end
 
-local orientation = getOrientation()
+local orientation = func.getOrientation()
 
 local x, z = client.locate()
 
