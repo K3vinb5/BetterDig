157e69ba2bd13ca02196aefe8310f02fb59d8b15
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2024-03-26 02:31:04 +0000

    Update install.lua

--- myGps/install/install.lua
+++ myGps/install/install.lua
@@ -5,9 +5,9 @@ shell.run("cd myGps")
 --main
 shell.run("mkdir main")
 shell.run("cd main")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/myGps/main/main/client.lua")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/myGps/main/main/server.lua")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/myGps/main/main/setup.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/myGps/main/client.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/myGps/main/server.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/myGps/main/setup.lua")
 shell.run("cd ..")
 shell.run("cd ..")
 if args[1] ~= nil then
