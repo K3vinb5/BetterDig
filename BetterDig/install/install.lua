3c252417376b91b82ab49f2f609b58eeff53adcb
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2024-03-26 02:29:01 +0000

    updated installer

--- BetterDig/install/install.lua
+++ BetterDig/install/install.lua
@@ -8,21 +8,21 @@ shell.run("cd BetterDig")
 --lib
 shell.run("mkdir lib")
 shell.run("cd lib")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/BetterDig/main/lib/func.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/BetterDig/main/lib/func.lua")
 shell.run("cd ..")
 --main
 shell.run("mkdir main")
 shell.run("cd main")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/BetterDig/main/main/betterDig.lua")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/BetterDig/main/main/setup.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/BetterDig/main/betterDig.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/BetterDig/main/setup.lua")
 --shell.run("pastebin get uPSuc3vU connect_display.lua")
 shell.run("cd ..")
 --startup
 shell.run("cd ..")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/BetterDig/main/install/startup.lua")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/BetterDig/main/install/setBetterDig.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/BetterDig/main/install/startup.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/BetterDig/main/install/setBetterDig.lua")
 --myGps
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/myGps/main/install/install.lua installmyGps")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/BetterDig/main/myGps/main/install/install.lua installmyGps")
 shell.run("installmyGps 0")
 shell.run("rm installmyGps")
 --seting up the turtle
